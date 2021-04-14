7203401b1ad0265ec183ae4de2317318d08dc80c
Linete Estrada <lye1@txstate.edu>
2021-04-13 22:05:47 -0500

    create navigation component and added it to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponentComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
